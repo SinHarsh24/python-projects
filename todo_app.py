cb89e29856766f42b216558b76a0f55035312976
Harsh Kumar <73462218+SinHarsh24@users.noreply.github.com>
2024-07-13 06:11:36 +0000

    Add script to capture to-do items with infinite loop and capitalize each input

--- todo_app.py
+++ todo_app.py
@@ -1,16 +1,10 @@
 # Prompt message to be displayed when asking for user input
 user_prompt = "Enter a todo :"
 
-# Taking input from the user for three to-do items
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
+todos = []
 
-# Creating a list of to-do items along with a predefined string "Hello"
-todos = [todo1, todo2, todo3, "Hello"]
+while True :
+    todo = input(user_prompt)
+    print(todo.capitalize)
+    todos.append(todo)
 
-# Printing the list of to-do items
-print(todos)
-
-# Printing the type of the first to-do item (which will be a string)
-print(type(todo1))
